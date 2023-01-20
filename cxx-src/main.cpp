a54a8ab522a2b1dd2207b82834aef3c3c7445e18
Federico Roux <rouxfederico@gmail.com>
2023-01-20 09:30:07 -0300

    Shared Mem as Template (#3)
    * init var as template
    
    * submod update and cmake for only header class
    
    * mini struct example on C++ shared memory
    
    * deleted unused files
    
    * 2 fields struct demo
    
    Co-authored-by: Federico Roux <federico.roux@globant.com>

--- cxx-src/main.cpp
+++ cxx-src/main.cpp
@@ -20,19 +20,31 @@
  * @return int 
  */
 
+typedef struct {
+    int num;
+    char c;
+} d_t;
+
 int main(int argc, char *argv[]) {
 
-    shmem sh1{argv[0]};
+    shmem<d_t> sh1(argv[0]);
 
     assert(!sh1.init());
+
+
+    d_t var;
+
+    var.num = 44;
+    var.c = 'x';
     
     /* read or modify the segment, based on the command line: */
     if (argc == 2) {
-        std::cout << "writing to segment: \"" << argv[1] << "\"" << std::endl;
-        strncpy(sh1.get_pointer(), argv[1], sh1.get_max_size());
+        std::cout << "writing to segment: "  << var.num << " - " << var.c << std::endl;
+        sh1.write(var);
     } 
     else {
-        printf("segment contains: \"%s\"\n", sh1.get_pointer());
+        auto res = sh1.read();
+        std::cout << "segment contains: "<< res.num << " - " << res.c << std::endl;
     }
 
     return 0;
